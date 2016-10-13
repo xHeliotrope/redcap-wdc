fdc03621ab86c767d4680ef1a54eb35d8127dc37
Ryan Moore <rmoore@dom.wustl.edu>
2016-10-13 14:51:05 -0500

    connectionData correctly jsonified/parsed

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const https = require('https');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/redcap_wdc.html');
+});
+app.post('/', function(req, res){
+    res.sendFile(__dirname + '/redcap_wdc.html');
+});
+
+port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('we left the backdoor open and they got right in!');
+});
